110a93d6c14e9b6bbe9230930e51dfdcf657486a
Bhoami K Khona <bhoamikhona@gmail.com>
2023-12-21 18:06:42 +0530

    navigation and statistics

--- planets-fact-site/starter-code/script.js
+++ planets-fact-site/starter-code/script.js
@@ -1,3 +1,8 @@
-const btnMobileMenu = document.querySelector(`.mobile-menu`);
+const btnMobileMenu = document.querySelector(`.btn-mobile-nav`);
+const nav = document.querySelector(`.main-nav`);
+const main = document.querySelector(`.main`);
 
-btnMobileMenu.addEventListener(`click`, function () {});
+btnMobileMenu.addEventListener(`click`, function () {
+  nav.classList.toggle("nav-open");
+  main.classList.toggle("hidden");
+});
