1ffd610fb492f9659e9fd38c9319a2151499a7fb
Bhoami K Khona <bhoamikhona@gmail.com>
2024-03-26 20:34:28 +0530

    audio player

--- dictionary-web-app/src/components/ResultHeader/ResultHeader.jsx
+++ dictionary-web-app/src/components/ResultHeader/ResultHeader.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ResultHeader.css";
 import { ReactComponent as PlayIcon } from "../../assets/images/icon-play.svg";
 
 function ResultHeader({ data }) {
-  const { word, phonetic } = data;
+  const { word, phonetic, phonetics } = data;
+  const [soundSrc, setSoundSrc] = useState("");
+
+  useEffect(() => {
+    if (phonetics && phonetics.length > 0) {
+      const audio =
+        phonetics.find((phonetic) => phonetic.audio !== "")?.audio || "";
+      setSoundSrc(audio);
+    }
+  }, [phonetics]);
+
+  const play = (e) => {
+    e.preventDefault();
+    if (soundSrc) {
+      new Audio(soundSrc).play();
+    }
+  };
+
   return (
     <div className="result-header">
       <div className="result-header__left">
@@ -11,8 +28,8 @@ function ResultHeader({ data }) {
         <span className="result-header__pronounce">{phonetic}</span>
       </div>
       <div className="result-header__right">
-        {word && (
-          <button className="play-btn">
+        {soundSrc && (
+          <button className="play-btn" onClick={play}>
             <PlayIcon className="play-icon" />
           </button>
         )}
