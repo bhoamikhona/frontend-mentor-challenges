e611b3872f10d484ff244528d7438f80f5dfe1d8
Bhoami K Khona <bhoamikhona@gmail.com>
2024-03-28 06:09:15 +0530

    search component UI

--- github-user-search-app/src/components/Search/Search.jsx
+++ github-user-search-app/src/components/Search/Search.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Search.css";
+import { ReactComponent as SearchIcon } from "../../assets/icon-search.svg";
 
 function Search() {
-  return <div>Search</div>;
+  const [error, setError] = useState(false);
+  return (
+    <form className="search-form">
+      <div className="search-icon-container">
+        <SearchIcon className="search-icon" />
+      </div>
+      <label htmlFor="search" className="search-label">
+        Search
+      </label>
+      <input
+        type="text"
+        className="search-input"
+        id="search"
+        placeholder="Search GitHub username..."
+        autoComplete="off"
+        spellCheck="off"
+      />
+      <span className={`search-error ${!error && "hidden"}`}>No results</span>
+      <button type="submit" className="search-btn">
+        Search
+      </button>
+    </form>
+  );
 }
 
 export default Search;
